bfb18692ef23ae3cac59c9be2d22b1a1c99964f6
Redwan9528 <redwandjedia23@gmail.com>
2025-11-20 17:18:40 +0100

    modif model user et db conn

--- Controllers/Controller_admin.php
+++ Controllers/Controller_admin.php
@@ -3,8 +3,10 @@
 class Controller_admin extends Controller {
 
     /* ajouter les autres méthodes ici sous la forme action_quelquechose */
+
     
     public function action_default() {
+        return $this->render("view_admin.php",);
         /* Méthode abstraite à reimplementer sinon ça marchera pas */
     }
 
---
+++ DB.php
@@ -0,0 +1,39 @@
+<?php 
+use PDO;
+use PDOException;
+class DB {
+
+    private static $pdo=null;
+
+
+private static function getConnectionDB() {
+
+    if (is_null(self::$pdo)) {
+
+      require __DIR__ . '/config.php';
+      try { 
+        self::$pdo =new PDO(
+            
+            "pgsql:host=" . DB_HOST. ";dbname=" . DB_NAME, DB_USER, DB_PASS,
+            [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                ]
+
+            );
+        } catch (PDOException $e) {
+            die("Erreur de connexion ") ;}
+        }
+
+        
+
+
+
+
+        return self::$pdo;
+    }
+    public static function get() {
+        return self::getConnectionDB();}
+}   
+
+?>
