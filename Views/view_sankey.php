479f8e8d46a92e39af6bfa137a81adce5d4e8e2d
Alvin Gosset <93733977+Algos25@users.noreply.github.com>
2025-12-01 18:39:15 +0100

    Enhance view_sankey.php with navigation and layout
    Added navigation and sections for Sankey diagram and stats.

--- Views/view_sankey.php
+++ Views/view_sankey.php
@@ -1,20 +1,22 @@
-<html>
+<!DOCTYPE html>
+<html lang="fr">
 <head>
-    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Sankey</title>
 </head>
 <body>
-  <header>
-
-  </header>
-    <main>
-      <div id="sankey-diagram">
-        <h3>Diagramme Sankey</h3>
-          <!-- Le diagramme Sankey sera affiché ici -->
-      </div>
-      <div id="Stats">
-        <!-- Les statistiques seront affichées ici -->
-      </div>
-    </main>
-
-    </body>
+    <nav><!-- Navbar -->
+        <a href="index.php?action=home"><img src="img/Retour.png"></a><!-- Boutton Retour navbar -->
+    </nav>
+    <section id="Sankey"><!-- Diagramme Sankey -->
+        <div id="sankey_container" class="flex justify-center mt-10">
+        </div>
+    </section>
+    <section id="stats"><!-- Affichage des stats -->
+        <input  class="items-start" type="checkbox">Afficher les stats <!-- Checkbox pour faire apparaitre les stats -->
+        <div class="container border bg-gray"></div><!-- Conteneur des stats -->
+    </section>
+</body>
 </html>
