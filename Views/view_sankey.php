f2c6bc8af89c205d2c29cda86423bedaae5f909b
Yasir-Manjra <yasir.manjra01@gmail.com>
2025-12-01 13:40:11 +0100

    Update view_sankey.php

--- Views/view_sankey.php
+++ Views/view_sankey.php
@@ -1,8 +1,20 @@
 <html>
 <head>
     <title></title>
-    <body>
-    <h1>Diagramme Sankey</h1>
-    <div id="sankey-diagram"></div>
+</head>
+<body>
+  <header>
+
+  </header>
+    <main>
+      <div id="sankey-diagram">
+        <h3>Diagramme Sankey</h3>
+          <!-- Le diagramme Sankey sera affiché ici -->
+      </div>
+      <div id="Stats">
+        <!-- Les statistiques seront affichées ici -->
+      </div>
+    </main>
+
     </body>
 </html>
