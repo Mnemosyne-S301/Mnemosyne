8007b09908cd428e31f83d3fe81162a1c5ec26c5
Alvin Gosset <atom13d443gix@gmail.com>
2025-12-05 14:56:11 +0100

    commit the changed files

--- Views/view_sankey.php
+++ Views/view_sankey.php
@@ -6,11 +6,11 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Sankey</title>
 </head>
-<body>
+<body class="bg-[#0A1E2F]">
     <nav><!-- Navbar -->
         <a href="index.php?action=home"><img src="img/Retour.png"></a><!-- Boutton Retour navbar -->
     </nav>
-    <section id="Sankey"><!-- Diagramme Sankey -->
+    <section id="Sankey" class="mt-6"><!-- Diagramme Sankey -->
         <div id="sankey_container" class="flex justify-center mt-10">
         </div>
     </section>
