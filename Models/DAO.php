67f10add4de3c0d7196ed427012fce9f42c467cf
Redwan9528 <redwandjedia23@gmail.com>
2025-12-10 16:34:27 +0100

    ajout fichier dao avec donnection db et api + ajout etudiantDAO

---
+++ Models/DAO.php
@@ -0,0 +1,66 @@
+<?php 
+require_once __DIR__ . "DB.php";
+
+abstract class DAO {
+private PDO $pdo;
+private string $token ;
+private string $api_url="https://ton-scodoc.fr/ScoDoc/api";
+
+
+    protected function __getdbconnction(): PDO {
+        $this->pdo = DB ::__getdbconnction();
+        return $this->pdo;  
+
+    }
+
+    protected function getToken () {// on s'authentifie a l api scodoc avec la methode curl, en faisant une requete pour le token avec mdp et login 
+
+        if ($this->token !== null) {// on regarde si on a deja un token 
+            return; 
+    } 
+
+    $curl= curl_init();
+    //obtenir un token si on en a pas 
+    curl_setopt_array($curl,[
+    CURLOPT_URL => $this->api_url . '/tokens',
+    CURLOPT_POST => true,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => ['Content-Type: application/json'],
+    CURLOPT_POSTFIELDS => json_encode(["username"=>$this->username, "password"=>$this->password])
+    ]);
+    
+    
+    
+    
+  $reponse=curl_exec($curl) ;
+  
+  curl_close($curl);
+  $json=json_decode($reponse,true);
+  $this->token = $json["token"];
+
+
+
+    
+}
+    protected function get(string $url):array{ // la fonction elle appelle l api avec l url souhaite, comme ca on appelle cette methode pour recup info sur par exemple etuidfiant 
+    $this->getToken();
+    $curl= curl_init();
+    curl_setopt_array($curl,[
+    CURLOPT_URL => $this->api_url . $url,
+    
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => [ "Authorization: Bearer {$this->token}", "Accept: application/json"]
+]);
+
+    $reponse=curl_exec($curl) ;\
+    curl_close($curl);
+    return json_decode($reponse,true);
+
+}
+   abstract public function findall();
+
+
+}
+
+
+?>
---
+++ Models/EtudiantDAO.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once __DIR__ ."/Etudiant.php";
+require_once __DIR__ ."/DAO.php";
+
+
+class EtudiantDAOApi extends DAO {
+    public function __construct() { $this->getToken();}
+
+    
+
+
+    public function findall() {return $this->get("/etudiants");}
+    public function findbyid(string $id) {return $this->get("/etudiants/" . $id);}
+
+}
+
+class EtudiantDAOBd extends DAO {
+    public function __construct()  {$this->__getdbconnction();}
+    
+    public function findall(){
+        $conn=$this->__getdbconnction();
+        $etudiants =$conn->prepare("SELECT * FROM Etudiant;");
+        $etudiants->execute();
+        $etudiants=$etudiants->fetchAll(PDO::FETCH_ASSOC);
+        $instances=[];
+        foreach ($etudiants as  $etudiant) {
+            $instances[]=new Etudiant($etudiant);}
+            
+
+        return $instances;
+            
+
+
+    }
+    public function findbyid(string $id) {
+           
+        $conn=$this->__getdbconnction();
+        $etudiant =$conn->prepare("SELECT * FROM Etudiant WHERE etudiant_id= :etudiant_id;");
+        $etudiant->execute(["etudiant_id"=> $id]);
+        $etudiant=$etudiant->fetch(PDO::FETCH_ASSOC);
+        
+        if ($etudiant){return new Etudiant($etudiant);}
+        else {echo "Aucun Etudiant avec cette id !";}
+            
+
+
+    }
+
+
+
+
+    
+}
