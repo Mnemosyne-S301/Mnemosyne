72d2b68f8c24084c9b56dd0b22226fc51ede1605
Redwan9528 <redwandjedia23@gmail.com>
2025-12-11 16:29:46 +0100

    controller admin

--- Controllers/Controller_admin.php
+++ Controllers/Controller_admin.php
@@ -3,15 +3,12 @@
 class Controller_admin extends Controller {
 
     /* ajouter les autres méthodes ici sous la forme action_quelquechose */
-<<<<<<< HEAD
-    
-    public function action_default() {
-=======
+
 
     
     public function action_default() {
         return $this->render("view_admin.php",);
->>>>>>> logAdmin
+
         /* Méthode abstraite à reimplementer sinon ça marchera pas */
     }
 
