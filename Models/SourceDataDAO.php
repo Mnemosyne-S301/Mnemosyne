865ac56e99da3a90b7524ed24af7672d95977fa5
JMLMath <jmlmatheo@icloud.com>
2025-12-11 17:16:54 +0100

    Added SourceDataDAO interface. Fait en voc discord. Pas fini.

--- Models/SourceDataDAO.php
+++ Models/SourceDataDAO.php
@@ -1,4 +1,16 @@
 <?php
 
+interface SourceDataDAO {
+    public function findall_etudiant();
+    public function findall_ue();
+    public function findall_departement();
+    public function findall_formsemestre();
+    public function findall_decision();
+    public function findformation_by_accronyme(string $accronyme);
+    public function finddepartement_by_accronyme(string $accronyme);
+    public function findformsemestre_by_id(string $id);
+
+    
+}
 
 ?>
