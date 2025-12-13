d30c7db7993fda5a76e6aab1c4c0841d9aaceb8e
Redwan9528 <redwandjedia23@gmail.com>
2025-12-13 13:44:13 +0100

    implementation de toutes les methodes de l interface dans scodocDAO et deplacement du fichier qui initialise la connexion avec la bd dans le dossier Models + modification du nom de la methode findformationby_acronyme en find by id

--- Models/SourceDataDAO.php
+++ Models/SourceDataDAO.php
@@ -6,7 +6,7 @@ interface SourceDataDAO {
     public function findall_departement();
     public function findall_formsemestre();
     public function findall_decision();
-    public function findformation_by_accronyme(string $accronyme);
+    public function findformation_by_id(string $id);
     public function finddepartement_by_accronyme(string $accronyme);
     public function findformsemestre_by_id(string $id);
 
