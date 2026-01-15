896ea46f1c6479a2cfea3bc9da642bda79ef4a82
Yasir-Manjra <yasir.manjra01@gmail.com>
2026-01-15 17:07:07 +0100

    modification dans le controller accueil et creation des getter dans le model formation

--- Controllers/Controller_accueil.php
+++ Controllers/Controller_accueil.php
@@ -5,6 +5,8 @@ class Controller_accueil extends Controller {
     /* ajouter les autres méthodes ici sous la forme action_quelquechose */
     
     public function action_default() {
+    /*  $nomdesformation il faudra le récupérer depuis la BDD plus tard */
+    /*  $annéformation il faudra recuperer depuis la BDD les années ou les formation ont eu lieu */
         $this->render('accueil');
     }
 
--- Models/Formation.php
+++ Models/Formation.php
@@ -14,4 +14,38 @@ class Formation{
         $this->rcues = $dict['rcues'];
         $this->competences = $dict['competences'];
     }
+
+    /* Getters */
+    public function getLabel(){
+        return $this->label;
+    }
+
+    public function getDepartement(){
+        return $this->departement;
+    }
+
+    public function getStartDate(){
+        return $this->startDate;
+    }
+
+    public function getRcues(){
+        return $this->rcues;
+    }
+
+    public function getCompetences(){
+        return $this->competences;
+    }
+
+    /* Other methods */
+    public function toDict() : array {
+        return array(
+                    'label' => $this->label,
+                    'departement' => $this->departement,
+                    'startDate' => $this->startDate,
+                    'rcues' => $this->rcues,
+                    'competences' => $this->competences
+                );
+
+    }
 }
+?>
