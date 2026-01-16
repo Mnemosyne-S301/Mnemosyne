a20e44664e509b3f6973a66f11e3dbe429481b96
Redwan9528 <redwandjedia23@gmail.com>
2026-01-16 19:44:19 +0100

    service api

--- Services/Service_api.php
+++ Services/Service_api.php
@@ -1,5 +1,81 @@
 <?php
+
+include_once __DIR__ . "/../Models/StatsDAO.php";
 class Service_api {
-    /* ecrire les fonctions ici */
+    private StatsDAO $dao;
+    public function __construct (?StatsDAO $dao=null){
+        $this->dao=$dao ?? StatsDAO::getModel();
+
+    }
+
+    public function ChaineVide(string $chaine){
+        $chaine=trim((string)$chaine);
+        if ($chaine ===""){
+            throw new InvalidArgumentException("Valeur null");
+        }
+        return $chaine;
+    }
+    public function ExigeAnnee($annee){
+        $annee=trim((string)$annee);
+        if (!preg_match('/^\d{4}(-\d{4})?$/',$annee)){
+            throw new InvalidArgumentException("Annee invalide");
+
+        }
+        return $annee;
+
+    }
+
+    public function getEleveParFormation($formation){
+        $formation=$this->ChaineVide($formation);
+        return $this->dao->getNbEleveParFormation($formation);
+
+    }
+
+    public function getNbEleveParAnnee($annee){
+        $annee=$this->ExigeAnnee($annee);
+        return $this->dao->getNbEleveParAnnee($annee);
+
+    }
+
+    public function getNbEleveParParcours($parcours){
+        $parcours=$this->ChaineVide($parcours);
+        return $this->dao->getNbEleveParParcours($parcours);
+
+    }
+     public function getNbRepartitionUEADMISParParcours($parcours){
+        $parcours=$this->ChaineVide($parcours);
+        return $this->dao->getNbRepartitionUEADMISParParcours($parcours);
+
+    }
+
+     public function getNbRepartitionUEADMISParFormation($formation){
+        $formation=$this->ChaineVide($formation);
+        return $this->dao->getNbRepartitionUEADMISParFormation($formation);
+
+    }
+
+    public function getNbRepartitionUEADMISParAnnee($annee){
+        $annee=$this->ExigeAnnee($annee);
+        return $this->dao->getNbRepartitionUEADMISParAnnee($annee);
+
+    }
+    public function getEffectifTotalPrecise($formation, $annee){
+    $annee=$this->ExigeAnnee($annee);
+    $formation=$this->ChaineVide($formation);
+    return $this->dao->getEffectifTotalPrecise($formation,$annee);
+
+    }
+
+  
+
+    
 }
+
+
+
+
+
+
+
+
 ?>
