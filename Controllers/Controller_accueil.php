941a38af9bcbd08eac6e6c75b6ff1f229cf9ee4b
hariz0212 <194072635+hariz0212@users.noreply.github.com>
2026-01-18 16:56:35 +0100

    attendte

--- Controllers/Controller_accueil.php
+++ Controllers/Controller_accueil.php
@@ -1,12 +1,19 @@
 <?php
 
+require_once ("Services/Service_scodoc.php");
+
 class Controller_accueil extends Controller {
+    private Service_scodoc $service_scodoc;
+
 
     /* ajouter les autres méthodes ici sous la forme action_quelquechose */
     
     public function action_default() {
-        $this->render('accueil');
+        session_start();
+        return $this->render('accueil');
     }
+
+    
 
 }
 ?>
